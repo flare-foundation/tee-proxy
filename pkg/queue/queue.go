39293fb2b24850098911524b20d0f8acfbbf6cef
Gregor Podlogar <gregor.podlogar@flare.network>
2025-10-06 13:29:48 +0200

    set expiration (30 days) on actions

--- pkg/queue/queue.go
+++ pkg/queue/queue.go
@@ -55,7 +55,7 @@ func NewActionQueues(client *redis.Client) *ActionQueues {
 func (as *ActionQueues) Enqueue(ctx context.Context, action *types.Action, queueID processorutils.QueueID) error {
 	id := StoringID{ActionID: action.Data.ID, SubmissionTag: action.Data.SubmissionTag}
 
-	err := as.actions.Set(ctx, id.String(), action)
+	err := as.actions.SetWithTTL(ctx, id.String(), action, 30*24*time.Hour)
 	if err != nil {
 		return err
 	}
@@ -95,7 +95,7 @@ func (as *ActionQueues) Dequeue(ctx context.Context, id processorutils.QueueID) 
 		return nil, fmt.Errorf("queued action not found: %s", storingID.String())
 	}
 
-	as.actions.Remove(ctx, storingID.String()) //errcheck // error can not happen because ee check that there is an item to remove.
+	as.actions.Remove(ctx, storingID.String()) //errcheck // error can only happen if context is canceled
 
 	return action, err
 }
